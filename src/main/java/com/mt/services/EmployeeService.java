43720076239de3e99012faa5e89ca50b6cb6a9f4
Vineet009 <vineet009@gmail.com>
2024-11-12 20:22:14 +0530

    Modify the name of properties

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,10 +22,10 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
+		js.put("Name", "Vineet Sharma");
+		js.put("Calling Name", "Vicky");
+		js.put("DOB", "12-Nov-2024");
+		js.put("Hobbies", "Reading Technical Blogs, Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
 
 		return js.toString();
